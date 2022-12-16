43862acb66358bddae25c5e001fe4c82ee97b5d3
alejandrodgz <5199@holbertonstudents.com>
2022-12-16 18:21:13 -0500

    task last

--- api/v1/app.py
+++ api/v1/app.py
@@ -7,7 +7,7 @@ from flask import Flask, make_response, jsonify
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app,  resources={r"/*": {"origins": "0.0.0.0"}})
+CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 app.register_blueprint(app_views)
 
 
@@ -16,10 +16,12 @@ def teardown(exception):
     """close the storage instance"""
     storage.close()
 
+
 @app.errorhandler(404)
 def not_found(error):
     """404 not found"""
     return make_response(jsonify({"error": "Not found"}), 404)
+
 
 if __name__ == "__main__":
     host = os.getenv('HBNB_API_HOST', default="0.0.0.0")
