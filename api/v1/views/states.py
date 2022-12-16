4e9c6d633ed79074c802831b0197a629758b8c23
alejandrodgz <5199@holbertonstudents.com>
2022-12-15 23:55:44 -0500

    task

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -63,4 +63,4 @@ def states_put(state_id):
         if key not in ["id","created_at","updated_at"]:
             setattr(obj, key, value)
     obj.save()
-    return make_response(jsonify(obj.to_dict(), 200))
+    return make_response(jsonify(obj.to_dict()), 200)
