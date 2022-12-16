b70db73ef1da43521ee0ed7fc3ff11f37616cf4c
alejandrodgz <5199@holbertonstudents.com>
2022-12-15 20:43:44 -0500

    task

--- api/v1/app.py
+++ api/v1/app.py
@@ -4,7 +4,6 @@ import os
 from api.v1.views import app_views
 from models import storage
 from flask import Flask, make_response, jsonify
-from flask_cors import CORS
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
@@ -22,4 +21,4 @@ def not_found(error):
 if __name__ == "__main__":
     host = os.getenv('HBNB_API_HOST', default="0.0.0.0")
     port = os.getenv('HBNB_API_PORT', default=5000)
-    app.run(host=host, port=port, threaded=True)
+    app.run(host=host, port=port, debug=True, threaded=True)
--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -3,23 +3,58 @@ from flask import jsonify, request, make_response
 from models import storage
 from models.state import State
 
-@app_views.route('/states', methods =['GET'], defaults={'state_id':None})
-@app_views.route('/states/<state_id>', methods=['GET'])
-def getState(state_id):
-    """just hoping this is a mistake"""
-    list_objects = []
-    if state_id and storage.get(State, "state_id") is None:
-        return make_response(jsonify({"error": "Not found"}), 404)
-    for i in storage.get(State, state_id):
-        list_objects.append(i)
-        return jsonify(list_objects)
 
-@app_views.route('/states/<state_id>', methods=['DELETE'])
-def statusDeletes(state_id):
-    """statusDeletes delete a state"""
-    if storage.get(State, state_id):
-            storage.delete(storage.get(State, state_id))
-            storage.save()
-            return make_response(jsonify({}, 200))
-    else:
+@app_views.route('/states', methods=['GET'], defaults={"state_id":None}, strict_slashes=False)
+@app_views.route('/states/<state_id>', methods=['GET'], strict_slashes=False)
+def states_get(state_id):
+    """ocumentation"""
+
+    list_obj = []
+    if state_id and storage.get(State, state_id) is None:
         return make_response(jsonify({"error": "Not found"}), 404)
+
+    if state_id:
+        return jsonify((storage.get(State, state_id).to_dict()))
+    else:
+        for i in storage.all(State).values():
+            list_obj.append(i.to_dict())
+    return jsonify(list_obj)
+
+@app_views.route('/states/<state_id>', methods=['DELETE'], strict_slashes=False)
+def states_delete(state_id):
+    if storage.get(State, state_id) is None:
+        return make_response(jsonify({"error": "Not found"}), 404)
+    obj = storage.get(State, state_id)
+    storage.delete(obj)
+    storage.save()
+    return make_response(jsonify({}), 200)
+
+
+@app_views.route('/states/<state_id>', methods=['POST'], strict_slashes=False)
+def states_post():
+    obj = request.get_json(silent=True)
+    if obj is None:
+        make_response(jsonify({"error": "Not found"}), 404)
+    if  not obj["name"]:
+        make_response(jsonify({"error": "Not found"}), 404)
+    newState = State()
+    for key, value in obj.items():
+        setattr(newState, key, value)
+    storage.new(newState)
+    storage.save()
+    return make_response(jsonify(newState.to_dict()), 200)
+
+@app_views.route('/states/<state_id>', methods=['PUT'], strict_slashes=False)
+def states_put(state_id):
+    json_obj = request.get_json(silent=True)
+    if json_obj is None:
+        make_response(jsonify({"error": "Not found"}), 404)
+    if  not json_obj["name"]:
+        make_response(jsonify({"error": "Not found"}), 404)
+    obj = storage.get(State, state_id)
+    if obj is None:
+        return make_response(jsonify({"error":"Not found"}), 404)
+    for key, value in json_obj.items():
+         setattr(obj, key, value)
+    obj.save()
+    return (jsonify(obj.to_dict()), 200)
